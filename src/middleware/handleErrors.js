5efb04f71c6c448c948265b32ec66c4a37c30d6a
Adam Katora <adamkatora@gmail.com>
2021-06-10 20:36:48 -0400

    Fix instanceof check order

--- src/middleware/handleErrors.js
+++ src/middleware/handleErrors.js
@@ -3,21 +3,23 @@
  * Thanks, Kevin: https://codeburst.io/better-error-handling-in-express-js-b118fc29e9c7
  */
 const path = require('path');
-const { GeneralError, DuplicateUser } = require(path.resolve('src','utils','errors'));
+const { GeneralError, BadRequest, DuplicateUser } = require(path.resolve('src','utils','errors'));
 
 const handleErrors = (err, req, res, next) => {
+    if (err instanceof DuplicateUser) {
+      return res.status(err.getStatusCode()).json({
+        status: 'error',
+        message: 'A user with that email already exists'
+      })
+    }
+
+    // This will return true for all errors since they inherit from GeneralError
+    // so put it last in the checks
     if (err instanceof GeneralError) {
       return res.status(err.getStatusCode()).json({
         status: 'error',
         message: err.message
       });
-    }
-
-    if (err instanceof DuplicateUser) {
-      return res.status(err.getStatusCode()).json({
-        status: 'error',
-        message: err.message
-      })
     }
   
     return res.status(500).json({
